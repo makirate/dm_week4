10fe6eb41eb3418c06c1272d4005eaa79506cde7
HaruhKi <mik.vanrgby@gmail.com>
2020-10-22 19:28:57 +0900

    to csv machine number and trouble

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 import sys
-import tkinter as tk
 import pandas as pd
-import tkinter.ttk as ttk
 import smtplib
 from email.mime.text import MIMEText
 import datetime
 
 # 不具合一覧
-# INOPERATION
+# INOPERATION 点火
 # WORN OUT
 # SUPPLY
-# DISCHARGER
+# DISCHARGER 放電
+# LIGHTNING 落雷
+# BIRD バードストライク
+# RECLINING 取り替え
+
+TROUBLE = ["INOPERATION","WORN","SUPPLY","DISCHARGER","LIGHTNING","BIRD","RECLINING"]
+MACHINE_NUMBER = ["01RK","02RK","03RK","04RK","05RK","06RK"]
 
 file = pd.read_csv('data.csv')
 file = file.dropna(how = "all",axis=1)
 file = file.dropna(how = "all")
-print("不具合情報:")
-value = input()
-value = value.upper()
-inpe = file[file['不具合情報'].str.match('^.*'+value+'.*$')]
-inpe_count = len(inpe)
-
-
+# print("不具合情報:")
+# value = input()
+# value = value.upper()
 d = datetime.datetime.today()
 date = d.strftime("%Y-%m-%d")
+
+
+def trouble_search (file, number):
+    for key in TROUBLE:
+        inpe = file[file['不具合情報'].str.match('^.*' + key + '.*$')]
+        inpe_count = len(inpe)
+        inpe.to_csv('to_csv_out_' + number + '_' + key + '.csv')
+        if (inpe_count >= 3):
+            print(number + "の" + key + "の不具合は3回以上です。")
+
+    return inpe_count
+
+for key in MACHINE_NUMBER:
+    inpe = file[file['機番'].str.match('^.*' + key + '.*$')]
+    # inpe.to_csv('to_csv_out_' + key + '_' + date + '.csv')
+    inpe_count =  trouble_search(inpe,key)
+    if (inpe_count > 3):
+        inpe.to_csv('to_csv_out_' + key + '_' + '.csv')
+
+
+
 
 # def send_mail():
 #     try:
@@ -49,11 +70,11 @@ date = d.strftime("%Y-%m-%d")
 #     except:
 #         return False
 
-print(inpe,"\n",inpe_count,"\n",value)
-
-if (inpe_count >= 3):
-    inpe.to_csv('to_csv_out'+date+'.csv')
-    print("3回以上です")
+# print(inpe,"\n",inpe_count,"\n",value)
+#
+# if (inpe_count >= 3):
+#     inpe.to_csv('to_csv_out'+date+'.csv')
+#     print("3回以上です")
     # try:
     #     if (send_mail()):
     #         print("Successfully sent email")
@@ -68,32 +89,3 @@ if (inpe_count >= 3):
 # inpe = file[file['不具合情報'].str.match('^.*INOPERATION.*$')]
 # print(inpe)
 
-
-# def search(event):
-#     value = EditBox.get()
-#     inpe = file[file['不具合情報'].str.match('^.*'+value+'.*$')]
-#     inpe_count = len(inpe)
-#     pd.set_option("display.max_columns", None)
-#     pd.set_option("display.max_rows", None)
-#     Static1 = tk.Label(text=inpe)
-#     Static1.pack()
-#     Static1.place(x=10, y=200)
-#
-#     Static1 = tk.Label(text=inpe_count)
-#     Static1.pack()
-#     Static1.place(x=10, y=100)
-#
-# root = tk.Tk()
-# root.title(u"Software Title")
-# root.geometry("1200x900")
-# EditBox = tk.Entry(width=50)
-# EditBox.pack()
-#
-# # value = EditBox.get()
-#
-# Button = tk.Button(text=u'button', width=50)
-# Button.bind("<Button-1>",search)
-# Button.pack()
-#
-#
-# root.mainloop()
